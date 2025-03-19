697ee5c635f2c39dee78a3871b615bf0cddfdc62
DpkShinde <deepak.shinde@financeshastra.com>
2025-03-19 13:22:21 +0530

    updated subscription

--- admin-dashboard/components/Navbar/Navbar.tsx
+++ admin-dashboard/components/Navbar/Navbar.tsx
@@ -6,7 +6,11 @@ import Image from "next/image";
 import FinanceShastraLogo from "./fs-white.png";
 import "./Navbar.css";
 
-const AdminNavbar: React.FC = () => {
+interface NavbarProps {
+    setActiveComponent: (component: string) => void;
+}
+
+const AdminNavbar: React.FC<NavbarProps> = ({ setActiveComponent }) => {
     const router = useRouter();
 
     const handleLogout = () => {
@@ -24,28 +28,54 @@ const AdminNavbar: React.FC = () => {
                 <BootstrapNavbar.Toggle aria-controls="navbarNav" />
                 <BootstrapNavbar.Collapse id="navbarNav" className="justify-content-between">
                     <Nav className="me-auto">
-                        <Link href="/users" passHref legacyBehavior><Nav.Link>User</Nav.Link></Link>
-                        <Link href="/recommendations" passHref legacyBehavior><Nav.Link>Recommendations</Nav.Link></Link>
+                        <Link href="/users" passHref legacyBehavior>
+                            <Nav.Link>User</Nav.Link>
+                        </Link>
+                        <Link href="/recommendations" passHref legacyBehavior>
+                            <Nav.Link>Recommendations</Nav.Link>
+                        </Link>
 
                         <NavDropdown title="Subscription" id="navbarDropdownSubscription">
-                            <Link href="/dashboard/subscription/AddSubscription" passHref legacyBehavior><NavDropdown.Item>Add Subscription</NavDropdown.Item></Link>
-                            <Link href="/adminDashboard/manage-subscription" passHref legacyBehavior><NavDropdown.Item>Manage Subscription</NavDropdown.Item></Link>
+                            <NavDropdown.Item
+                                onClick={() => setActiveComponent("addSubscription")}
+                            >
+                                Add Subscription
+                            </NavDropdown.Item>
+                            <Link href="/adminDashboard/manage-subscription" passHref legacyBehavior>
+                                <NavDropdown.Item>Manage Subscription</NavDropdown.Item>
+                            </Link>
                         </NavDropdown>
 
-                        <Link href="/analyst-corner" passHref legacyBehavior><Nav.Link>Analyst Corner</Nav.Link></Link>
-
                         <NavDropdown title="Learn" id="navbarDropdownLearn">
-                            <Link href="/adminDashboard/create-blog" passHref legacyBehavior><NavDropdown.Item>Create Blog</NavDropdown.Item></Link>
-                            <Link href="/edit-blog" passHref legacyBehavior><NavDropdown.Item>Edit/Delete Blog</NavDropdown.Item></Link>
-                            <Link href="/create-news" passHref legacyBehavior><NavDropdown.Item>Create News</NavDropdown.Item></Link>
-                            <Link href="/edit-news" passHref legacyBehavior><NavDropdown.Item>Edit/Delete News</NavDropdown.Item></Link>
-                            <Link href="/create-course" passHref legacyBehavior><NavDropdown.Item>Create Course</NavDropdown.Item></Link>
-                            <Link href="/edit-course" passHref legacyBehavior><NavDropdown.Item>Edit/Delete Course</NavDropdown.Item></Link>
+                            <NavDropdown.Item
+                                onClick={() => setActiveComponent("createBlog")}
+                            >
+                                Create Blog
+                            </NavDropdown.Item>
+                            <Link href="/edit-blog" passHref legacyBehavior>
+                                <NavDropdown.Item>Edit/Delete Blog</NavDropdown.Item>
+                            </Link>
+                            <Link href="/create-news" passHref legacyBehavior>
+                                <NavDropdown.Item>Create News</NavDropdown.Item>
+                            </Link>
+                            <Link href="/edit-news" passHref legacyBehavior>
+                                <NavDropdown.Item>Edit/Delete News</NavDropdown.Item>
+                            </Link>
+                            <Link href="/create-course" passHref legacyBehavior>
+                                <NavDropdown.Item>Create Course</NavDropdown.Item>
+                            </Link>
+                            <Link href="/edit-course" passHref legacyBehavior>
+                                <NavDropdown.Item>Edit/Delete Course</NavDropdown.Item>
+                            </Link>
                         </NavDropdown>
 
                         <NavDropdown title="Company" id="navbarDropdownCompany">
-                            <Link href="/add-company" passHref legacyBehavior><NavDropdown.Item>Add Company</NavDropdown.Item></Link>
-                            <Link href="/manage-company" passHref legacyBehavior><NavDropdown.Item>Manage Company</NavDropdown.Item></Link>
+                            <Link href="/add-company" passHref legacyBehavior>
+                                <NavDropdown.Item>Add Company</NavDropdown.Item>
+                            </Link>
+                            <Link href="/manage-company" passHref legacyBehavior>
+                                <NavDropdown.Item>Manage Company</NavDropdown.Item>
+                            </Link>
                         </NavDropdown>
                     </Nav>
 
